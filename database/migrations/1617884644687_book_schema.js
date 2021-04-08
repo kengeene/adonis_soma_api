9c96e58f96b26a518ef7f3f62170a9e625ca6e53
Eugene Kuria <kengeene@gmail.com>
2021-04-08 15:29:57 +0300

    Add Model, Migrations, And Controller files for book

---
+++ database/migrations/1617884644687_book_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class BookSchema extends Schema {
+  up () {
+    this.create('books', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('books')
+  }
+}
+
+module.exports = BookSchema
